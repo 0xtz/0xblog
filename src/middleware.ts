050c4dc61c3e49b4008ffb025ae2e7b930396646
0xtz <0xtz.52@gmail.com>
2025-08-08 00:38:51 +0100

    ...

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { type NextRequest, NextResponse } from "next/server"
 const allowedOrigins = [
   "https://0xtz.me",
   "https://blog.0xtz.me",
-  "0xblog-rose.vercel.app",
+  "https://0xblog-rose.vercel.app",
 ]
 
 export function middleware(request: NextRequest) {
@@ -17,7 +17,7 @@ export function middleware(request: NextRequest) {
   })
 
   if (allowedOrigins.includes(origin)) {
-    requestHeaders.set("Access-Control-Allow-Origin", origin)
+    response.headers.set("Access-Control-Allow-Origin", origin)
   }
 
   response.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
